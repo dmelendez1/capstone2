0c6dddbfd723a7d31d867bf769ec97d0ce2b4ca6
melzmatz <melendezdiana95@gmail.com>
2023-11-20 15:06:27 -0700

    sav

--- scripts/second.js
+++ scripts/second.js
@@ -1,9 +1,16 @@
+// Function to fetch sunrise and sunset times
+async function getSunsetForMountain(lat, lng) {
+    let response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
+    let data = await response.json();
+    return data;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const dropdown = document.getElementById('mountainDropdown');
     const findButton = document.getElementById('findMtButton');
     const list = document.querySelector('.mt-display-module-list');
 
-    // populate the dropdown
+    // Populate the dropdown
     mountainsArray.forEach(mt => {
         let option = document.createElement('option');
         option.value = mt.name;
@@ -11,36 +18,37 @@ document.addEventListener('DOMContentLoaded', () => {
         dropdown.appendChild(option);
     });
 
-    // display top 6 mountains
+    // Display top 6 mountains
     displayMountains(mountainsArray.slice(0, 6));
 
-    // event listener for find button
-    findButton.addEventListener('click', function() {
+    // Event listener for find button
+    findButton.addEventListener('click', () => {
         const selectedMountain = mountainsArray.find(mt => mt.name === dropdown.value);
         displayMountains([selectedMountain]);
     });
 
-    function displayMountains(mountainsToShow) {
-        // clear existing content
+    // Function to display mountains
+    async function displayMountains(mountainsToShow) {
+        // Clear existing content
         while (list.firstChild) {
             list.removeChild(list.firstChild);
         }
 
-        mountainsToShow.forEach(mt => {
-            // create the list item
+        for (const mt of mountainsToShow) {
+            // Create the list item
             const li = document.createElement('li');
             li.className = 'trail-card';
 
-            // create the image
+            // Create the image
             const img = document.createElement('img');
             img.src = `images/${mt.img}`;
             img.alt = mt.name;
 
-            // create the info div
+            // Create the info div
             const infoDiv = document.createElement('div');
             infoDiv.className = 'info';
 
-            // create  append the name, elevation, effort, and description
+            // Create and append the name, elevation, effort, and description
             const h3 = document.createElement('h3');
             h3.textContent = mt.name;
 
@@ -53,18 +61,33 @@ document.addEventListener('DOMContentLoaded', () => {
             const pDesc = document.createElement('p');
             pDesc.textContent = mt.desc;
 
-            // append elements to the info div
+            // Fetch sunrise and sunset times
+            const sunTimes = await getSunsetForMountain(mt.lat, mt.lng);
+            const sunrise = sunTimes.results.sunrise;
+            const sunset = sunTimes.results.sunset;
+
+            // Create paragraphs for sunrise and sunset
+            const pSunrise = document.createElement('p');
+            pSunrise.textContent = `Sunrise: ${sunrise}`;
+
+            const pSunset = document.createElement('p');
+            pSunset.textContent = `Sunset: ${sunset}`;
+
+            // Append sunrise and sunset info to the info div
             infoDiv.appendChild(h3);
             infoDiv.appendChild(pElevation);
             infoDiv.appendChild(pEffort);
             infoDiv.appendChild(pDesc);
+            infoDiv.appendChild(pSunrise);
+            infoDiv.appendChild(pSunset);
 
-            // ppend the image and info div to the list item
+            // Append the image and info div to the list item
             li.appendChild(img);
             li.appendChild(infoDiv);
 
             // Append the list item to the list
             list.appendChild(li);
-        });
+        }
     }
-}); 
+});
+
