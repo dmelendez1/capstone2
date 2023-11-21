b1e15b20e4e2ad698775bd577b4a7cbe8772e799
melzmatz <melendezdiana95@gmail.com>
2023-11-21 00:29:29 -0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# capstone2
+# Wildscape
+
+## Overview
+Wildscape is an interactive web platform dedicated to exploring the natural beauty of US national parks and mountains. Our site offers a comprehensive guide for outdoor enthusiasts to browse, discover, and plan visits to various national parks and hiking trails across the United States.
+
+## Features
+
+### National Parks
+- **Explore Parks**: Users can explore a vast array of national parks. Each park is detailed with beautiful imagery, park history, visitor information, and tips for making the most of your visit.
+- **Search Functionality**: Easily search for parks based on states, activities, or park names.
+
+### Mountains to Hike
+- **Mountain Guides**: Detailed guides for a range of mountains suitable for various skill levels. From gentle hikes to challenging climbs, there's something for every adventurer.
+- **Interactive Maps**: Interactive maps to help users visualize the location and trails of the mountains.
+
+### User Interaction
+- **Plan Your Visit**: Tools and features to help users plan their visits, including difficulty level, sunset/sunrise.
+
+
+## Images
+![Sketch of homepage](images2/layout1.jpg)
+![Sketch of homepage](images2/layout2.jpg)
+![Sketch of homepage](images2/layout4.jpg)
+![Sketch of parks page](images2/layout5.jpg)
+![Sketch of parks page](images2/layout3.jpg)
+![Sketch of mountain page](images2/layout6.jpg)
+![Sketch of mountain javascript logic](images2/logic1.jpg)
+![Sketch of park javascript logic](images2/logic2.jpg)
--- scripts/main.js
+++ scripts/main.js
@@ -1,101 +1,116 @@
+
+// Add event listener for DOMContentLoaded to call initial setup functions
 document.addEventListener("DOMContentLoaded", () => { 
-    populateLocationDropdown();  // call my three functions
-    populateTypeDropdown();
-    displayTop10Parks();
+    populateLocationDropdown();  // Populate location dropdown with options
+    populateTypeDropdown();      // Populate park type dropdown with options
+    displayTop10Parks();         // Display the top 10 parks
 });
 
-function populateLocationDropdown() { //pops  a dropdown menu for selectring park locatrion. creates option in dropdown using array.
+// Function to populate the location dropdown
+function populateLocationDropdown() {
     const locationDropdown = document.getElementById('locationDropdown');
-    locationDropdown.onchange = handleLocationChange;
-    addOption(locationDropdown, "Location", true, true);
-    addOption(locationDropdown, "All", false, false);
+    locationDropdown.onchange = handleLocationChange; // Set onchange handler
+    addOption(locationDropdown, "Location", true, true); // Add default placeholder option
+    addOption(locationDropdown, "All", false, false);   // Add 'All' option
     locationsArray.forEach(location => {
-        addOption(locationDropdown, location, false, false);
+        addOption(locationDropdown, location, false, false); // Add each location to dropdown
     });
 }
 
+// Function to populate the park type dropdown
 function populateTypeDropdown() {
     const typeDropdown = document.getElementById('typeDropdown');
-    typeDropdown.onchange = handleTypeChange;
-    addOption(typeDropdown, "Park Type", true, true);
-    addOption(typeDropdown, "All", false, false);
+    typeDropdown.onchange = handleTypeChange; // Set onchange handler
+    addOption(typeDropdown, "Park Type", true, true); // Add default placeholder option
+    addOption(typeDropdown, "All", false, false);     // Add 'All' option
     parkTypesArray.forEach(type => {
-        addOption(typeDropdown, type, false, false);
+        addOption(typeDropdown, type, false, false); // Add each park type to dropdown
     });
 }
 
+// Function to display the top 10 parks
 function displayTop10Parks() {
     const resultsDiv = document.getElementById('searchResults');
-    resultsDiv.textContent = ''; // Clear previous results
+    resultsDiv.textContent = ''; // Clear any previous results
 
     top10ParksArray.forEach(park => {
-        appendParkDetails(resultsDiv, park);
+        appendParkDetails(resultsDiv, park); // Append details of each park
     });
 }
 
+// Helper function to add options to a dropdown
 function addOption(dropdown, text, isPlaceholder, isSelected) {
     const option = document.createElement('option');
     option.textContent = text;
     option.value = text;
-    if (isPlaceholder) option.disabled = true;
-    if (isSelected) option.selected = true;
+    if (isPlaceholder) option.disabled = true; // Disable option if it a placeholder
+    if (isSelected) option.selected = true;    // Set option as selected if required
     dropdown.appendChild(option);
 }
 
+// Placeholder function for handling location change
 function handleLocationChange() {
-    
+    // Functionality to be added for handling location dropdown change
 }
 
+// Placeholder function for handling park type change
 function handleTypeChange() {
-    
+    // Functionality to be added for handling park type dropdown change
 }
 
+// Add event listener to the 'find park' button
 document.getElementById('findParkButton').addEventListener('click', () => {
     const selectedLocation = document.getElementById('locationDropdown').value;
     const selectedType = document.getElementById('typeDropdown').value;
-    displayParks(selectedLocation, selectedType);
+    displayParks(selectedLocation, selectedType); // Display parks based on selected filters
 });
 
+// Function to display parks based on selected location and type
 function displayParks(selectedLocation, selectedType) {
     const resultsDiv = document.getElementById('searchResults');
-    resultsDiv.textContent = ''; // Clear previous results
+    resultsDiv.textContent = ''; // Clear any previous results
 
+    // Filter parks based on selected location and type
     const filteredParks = nationalParksArray.filter(park => 
         (selectedLocation === 'All' || park.State === selectedLocation) &&
         (selectedType === 'All' || park.LocationName.toLowerCase().includes(selectedType.toLowerCase()))
     );
 
+    // Display message if no parks are found
     if (filteredParks.length === 0) {
         resultsDiv.textContent = 'No parks found';
         return;
     }
 
+    // Append details of each filtered park
     filteredParks.forEach(park => {
         appendParkDetails(resultsDiv, park);
     });
 }
 
+// Function to append park details to a container
 function appendParkDetails(container, park) {
     const parkDiv = document.createElement('div');
 
+    // Create and append park name heading
     const nameHeading = document.createElement('h3');
     nameHeading.textContent = park.LocationName;
     parkDiv.appendChild(nameHeading);
 
+    // Create and append park address
     const addressParagraph = document.createElement('p');
     addressParagraph.textContent = `${park.Address}, ${park.City}, ${park.State}`;
     parkDiv.appendChild(addressParagraph);
 
-    // Check if Visit link is available nd add it
+    // Add visit link if available
     if (park.Visit) {
         const visitLink = document.createElement('a');
         visitLink.href = park.Visit;
-        visitLink.textContent = 'visit parks page';
-        visitLink.target = '_blank'; // To open link in a newtab
-        visitLink.style.display = 'block'; 
+        visitLink.textContent = 'Visit National Parks Website';
+        visitLink.target = '_blank'; // Opens link in a new tab
         parkDiv.appendChild(visitLink);
     }
 
+    // Append the park div to the container
     container.appendChild(parkDiv);
 }
-
